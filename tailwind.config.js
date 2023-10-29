e8a4340f99e81b6c221bccd8086ea7089d57a908
GhazwanDewa <dewamuchari12@gmail.com>
2023-10-29 20:43:08 +0700

    portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center:true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary:'#0ea5e9',
+        dark:'#0f172a',
+        secondary:'#64748b',
+      },
+      screens:{
+        '2xl':'1320px',
+      },
+      keyframes: {
+        goyang: {
+            '0%, 100%' : { transform: 'rotate(-3deg)'},
+            '50%' : { transform: 'rotate(3deg)'},
+        },
+    },
+  },
+  plugins: [],
+}
+}
